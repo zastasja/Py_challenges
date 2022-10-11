b9e86cefe43698e2a4f34c2bb0aadb6c30f04fb2
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-10-11 20:57:16 +0300

    Create div_numbers.py

---
+++ div_numbers.py
@@ -0,0 +1,16 @@
+#Какова сумма всех натуральных делителей числа 34?
+from functools import reduce
+
+n = int(input())
+i = 1
+d = []
+
+while i**2 <= n:
+  if n%i == 0:
+    d.append(i)
+    if i != n:
+      d.append(n//i)
+  i+=1
+d.sort()
+
+print(reduce(lambda x,y: x+y, d))
