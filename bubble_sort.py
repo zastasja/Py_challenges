c05a9d4ad253c63eef6b640cc5c30c16625f9dff
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-12-13 12:24:00 +0300

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+# bubble sort
+n = 6
+nums = [5, 7, 4, 3, 8, 2]
+count = 0
+for run in range(n - 1):
+    for i in range(n - 1):
+        if nums[i] > nums[i + 1]:
+            count += 1
+            nums[i], nums[i + 1] = nums[i + 1], nums[i]
+
+print(nums)
+print(count)
