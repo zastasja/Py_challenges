df7aa74948548d6a3948c53e1ffe0fba03ba6d6a
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-12-13 12:41:19 +0300

    bubble sorting algorithm
    added input

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,13 @@
 # bubble sort
-n = 6
-nums = [5, 7, 4, 3, 8, 2]
+n = int('Введите количество чисел', input())
+nums = list(map(int,'Введите числа для сортировки через пробел', input().split()))
 count = 0
 for run in range(n - 1):
-    for i in range(n - 1):
+    for i in range(n - 1 - run):
         if nums[i] > nums[i + 1]:
             count += 1
             nums[i], nums[i + 1] = nums[i + 1], nums[i]
 
 print(*nums)
 print(count)
+
