6393522c060a851ceb2122c0540f296907581b07
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-12-13 16:56:44 +0300

    recursive sorting algorithms сортировка слиянием

---
+++ recursive_sorting_algo.py
@@ -0,0 +1,31 @@
+# recursive sorting algorithms сортировка слиянием
+s = list(map(int, input().split()))
+
+
+def merge_2_list(a, b):
+    c = []
+    i, j = 0, 0
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+    if i < len(a):
+        c += a[i:]
+    if j < len(b):
+        c += b[j:]
+    return c
+
+
+def merge_sort(s):
+    if len(s) == 1:
+        return s
+    middle = len(s) // 2
+    left = merge_sort(s[:middle])
+    right = merge_sort(s[middle:])
+    return merge_2_list(left, right)
+
+
+print(*merge_sort(s))
