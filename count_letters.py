6cf509704e639cdea5abbc53bfd5d834a5f9cb2f
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-12-16 20:07:26 +0300

    посчитать повторение букв в слове

--- count_letters.py
+++ count_letters.py
@@ -9,3 +9,11 @@ for i in word:
     letters.append(i)
 print(count)
 print(letters)
+
+## решение для кириллицы:
+from collections import Counter
+
+word = input().lower()
+letters = Counter(word).most_common(1)
+s = str(*letters).split(',')
+print(s[1].replace(')', ''))
