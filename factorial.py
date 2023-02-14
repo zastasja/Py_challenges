36299920ca43600ea28990504009ebae7310a3cd
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-02-14 16:46:08 +0300

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -30,3 +30,19 @@ def factorial_small_num():
     fac = math.factorial(num)
     print(fac)
 factorial_num()
+
+#Example 3
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+for n in range(1, 6):  # testing
+    print(n, factorial_function(n))
+
