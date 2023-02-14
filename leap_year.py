24661dcd96a96f5cd0af7e61375c67b0f8281457
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-02-14 10:57:02 +0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -4,3 +4,23 @@ if year % 100 != 0 and year % 4 == 0 or year % 400 == 0:
     print('YES')
 else:
     print('NO')
+
+# Scenario
+# Your task is to write and test a function which takes one argument (a year) and returns True if the year is a leap year, or False otherwise.
+
+def is_year_leap(year):
+    if year % 100 != 0 and year % 4 == 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+for i in range(len(test_data)):
+	yr = test_data[i]
+	print(yr,"->",end="")
+	result = is_year_leap(yr)
+	if result == test_results[i]:
+		print("OK")
+	else:
+		print("Failed")
