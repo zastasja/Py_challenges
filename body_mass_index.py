d8edd5c27741379bf605b4a6da0fac745d64cc88
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-02-14 16:31:09 +0300

    Update body_mass_index.py

--- body_mass_index.py
+++ body_mass_index.py
@@ -1,6 +1,12 @@
 # simple function: evaluating the BMI (Body Mass Index)
 
 def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+    weight < 20 or weight > 200:
+        return None
+
     return weight / height ** 2
 
+
 print(bmi(52.5, 1.65))
+
